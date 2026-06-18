6455ed85c69aac9d8c2dca740950e80cced0985b
adityasharma <mahiadit4321@gmail.com>
2026-06-19 01:25:47 +0530

    Refactor input validation in name checking and list updating functions for improved handling of whitespace

--- main/main.py
+++ main/main.py
@@ -3,37 +3,36 @@ list = ["happy", "rohan", "aditya", "rahul", "sujal"]# A temperory list
 def name_check():
     while True:
         name = input("\nEnter your name(or 'q' to back to main menu): ").lower()
-        if not name.isalpha():
+        if not name.replace(" ", "").isalpha():
             print("Invalid input! Use non-numeric names...")
             continue
         if name in list:
             print("The given name is already present in the list!")
         elif name == 'q':
-            menu()
+            return
         else:
             print("Name is not present in the list")
 #list updating block
 def list_update():
     while True:
       name1 = input("\nEnter a name to add or (q to go to main menu): ").lower()
-      if not name1.isalpha():
+      if not name1.replace(" ", "").isalpha():
           print("Invalid input! Use non-numeric names...")
           continue 
       if name1 == "q":
           print("\nreturning to main menu..")
-          menu()
-          break
+          return
       list.append(name1)
       print(f"{name1} has been added to the list!")
 #menu block
 def menu():
     print('\n=================' \
     ' Welcome To Mark Registry App =================')
-    print('\n Menu')
-    print("\n(y) To check, if the name is present in the list or not.")
-    print("(n) To update or add a name in the list.")
-    print("(q) To exit.")
     while True:
+        print('\n Menu')
+        print("\n(y) To check, if the name is present in the list or not.")
+        print("(n) To update or add a name in the list.")
+        print("(q) To exit.")
         choice = (input("Choose: ")).lower()
         if choice == "y":
             name_check()
