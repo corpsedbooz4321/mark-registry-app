8e22da3e23344d6624c9f1ca5bbf5f26714be4f6
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-26 00:01:47 +0530

    refactor: streamline remove function and improve user input handling

--- main/features/delete_entry.py
+++ main/features/delete_entry.py
@@ -1,36 +1,23 @@
 # Script that holds the fuction removes or deletes the entry!!
 import json
-from re import sub
+from warnings import resetwarnings
 
+from ui.banner import box_line_downwards, box_line_upwards
 from ui.colors import BLUE, GREEN, MAGENTA, RED, RESET, YELLOW
 
-print("========== Removing entries ==========")
 
-def user_input(data):
-    name = input(f"{YELLOW}Enter your name: {RESET}").lower()
-    if name in data["name"]:
-        removal = input(f"{YELLOW}Enter which subject you want to remove: {RESET}")
-        if removal in data["name"][name]["subject"]:
-            del data["name"][name]["subject"][removal]
-            print(f"{GREEN}Data removal of {removal} from the {name} is done!!")
-    else:
-        print(f"{RED}subject not found!!{RESET}")
-            
+def remove():
+    print("========== Removing entries ==========")
 
-
-
-
-
-
-def delete():
     with open("maindata/data.json") as file:
         data = json.load(file)
-        user_input()
-        print(f"Data related to the {BLUE}{name}{RESET} found!!")
-        print(f"{GREEN}(D) To delete a student. {RESET}")
-        print(f"{GREEN}(E) To delete a student's entries. {RESET}")
-        choice = input(f"{YELLOW}Choice: {RESET}").lower()
-        if choice == 1:
-            pass
-        name = input(f"{YELLOW}Enter your name: {RESET}").lower()
-        if name in data[name]:
+
+    name = input(f"{YELLOW}Enter your name: {RESET}")
+    if name in data:
+        box_line_downwards()
+        print(f" {GREEN}Student{RESET} : {BLUE}{name}{RESET}")
+        for subject in data[name]:
+            print(f" {YELLOW}{subject}{RESET}: {MAGENTA}{data[name][subject]}{RESET}")
+        box_line_upwards()
+    subject_removal = input(f"{YELLOW}Enter subject: {RESET}")
+
