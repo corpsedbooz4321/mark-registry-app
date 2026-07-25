4c099753693a030aacd083b632ff048d4dbd4e44
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-25 12:16:02 +0530

    feat: enhance user input prompts with color formatting for better visibility

--- main/update.py
+++ main/update.py
@@ -1,42 +1,49 @@
 # update.py
 import json
 
+from banner import show_line, update_banner
+from colors import GREEN, MAGENTA, RED, RESET, YELLOW
+
 
 def update_result():
+    update_banner()
+    show_line()
     with open("data.json") as file:
         data = json.load(file)
     while True:
-        new_name = input("\nName to update(or 'q' to menu): ").lower()
+        new_name = input(f"\n{YELLOW}Name to update(or 'q' to menu): {RESET}").lower()
         if new_name == "q":
             return
         if not new_name.replace(" ", "").isalpha():
-            print("Invalid name!, use Non-numeric names...!!")
+            print(f"{RED}Invalid name!, use Non-numeric names...!!{RESET}")
             continue
         if new_name not in data:
             data[new_name] = {}
 
         while True:
-            subject = input("\nEnter the name of the subjects: ").lower()
+            subject = input(
+                f"\n{YELLOW}Enter the name of the subjects: {RESET}"
+            ).lower()
             if subject == "done":
                 with open("data.json", "w") as file:
                     json.dump(data, file, indent=2)
                 print("Finished..!!")
                 return
             if not subject.replace(" ", "").isalpha():
-                print(f"{subject}, Invalid subject name..")
+                print(f"{subject}, {RED}Invalid subject name..{RESET}")
                 continue
             while True:
 
                 try:
-                    mark = int(input("Enter marks: "))
+                    mark = int(input(f"{YELLOW}Enter marks: {RESET}"))
 
                 except ValueError:
-                    print("Invalid input, Marks must be a number!!")
+                    print(f"{RED}Invalid input, Marks must be a number!!{RESET}")
                     continue
                 if 0 <= mark <= 100:
                     data[new_name][subject] = mark
                     print(
-                        f"Success! {new_name}'s data has been added 'done' to save all the changes!"
+                        f"{GREEN}Success! {new_name}'s data has been added 'done' to save all the changes!{RESET}"
                     )
                     break
-                print("\nMarks must be between 0 and 100.")
+                print(f"\n{RED}Marks must be between 0 and 100.{RESET}")
