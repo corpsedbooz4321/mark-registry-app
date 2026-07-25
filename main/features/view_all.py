a835b151f8dbdda42a419e0955c862051708af37
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-25 17:26:27 +0530

    fix: enhance output formatting in view_data function for improved readability

--- main/features/view_all.py
+++ main/features/view_all.py
@@ -1,6 +1,7 @@
 import json
 
 from ui.banner import box_line_downwards, box_line_upwards
+from ui.colors import BLUE, GREEN, MAGENTA, RED, RESET, YELLOW
 
 
 def view_data():
@@ -8,8 +9,10 @@ def view_data():
         data = json.load(file)
     for name in data:
         box_line_downwards()
-        print(f"Student:  {name}")
+        print(f" {GREEN}Student     :{RESET} {BLUE}{name.capitalize()}{RESET}")
 
         for subject in data[name]:
-            print(f" {subject}: {data[name][subject]}")
+            print(
+                f" {YELLOW}{subject:<12}{RESET}: {MAGENTA}{data[name][subject]}{RESET}"
+            )
         box_line_upwards()
