e040e5584bd7ad1ffcebaacf77f9159e87a285fe
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-25 10:58:12 +0530

    feat: enhance result display with improved formatting and additional visual elements

--- main/check.py
+++ main/check.py
@@ -1,12 +1,17 @@
 # check.py
 import json
+import re
+
+from banner import (box_line_downwards, box_line_upwards, result_banner,
+                    show_line)
 
 
 def check_result():
     with open("data.json") as file:
         student_data = json.load(file)
+    result_banner()
+    show_line()
     while True:
-        print("\n===== Check your result by Entering your name =====")
         name = input("\nEnter your name(or press q for main Menu): ").lower()
         if name == "q":
             return
@@ -17,8 +22,10 @@ def check_result():
 
         if name in student_data:
             marks = student_data[name]
-            print(f"\nStudent: {name.title()}")
+            box_line_downwards()
+            print(f" Student: {name.title()}")
             for subject, mark in marks.items():
-                print(f"{subject.title()}: {mark}%")
+                print(f" {subject.title()}: {mark}%")
+            box_line_upwards()
         else:
             print("No results found with the given name!")
