397d43c51aa43353456e23a5a239b7fec6945b45
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-26 00:38:52 +0530

    fix: remove unused import and improve subject removal logic in delete_entry.py

--- main/features/delete_entry.py
+++ main/features/delete_entry.py
@@ -1,6 +1,5 @@
 # Script that holds the fuction removes or deletes the entry!!
 import json
-from warnings import resetwarnings
 
 from ui.banner import box_line_downwards, box_line_upwards
 from ui.colors import BLUE, GREEN, MAGENTA, RED, RESET, YELLOW
@@ -19,5 +18,15 @@ def remove():
         for subject in data[name]:
             print(f" {YELLOW}{subject}{RESET}: {MAGENTA}{data[name][subject]}{RESET}")
         box_line_upwards()
-    subject_removal = input(f"{YELLOW}Enter subject: {RESET}")
-
+        while True:
+            subject_removal = input(f"{YELLOW}Enter the name of the subject: {RESET}")
+            if subject_removal in data[name]:
+                del data[name][subject_removal]
+                print(
+                    f"{GREEN}Data removal of {subject_removal} from the {name} finished!'done' to save!"
+                )
+                continue
+            with open("maindata / data.json", "w") as file:
+                json.dump(data, file, indent=2)
+    else:
+        print(f"{RED}Subject not found!{RESET}")
