ee9a3ea0b2dc9a172bc73113275291b96dd06482
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-07-27 01:28:08 +0530

    refactor: remove unused import and improve input prompt formatting

--- Mark-Registry-app/main.py
+++ Mark-Registry-app/main.py
@@ -4,7 +4,7 @@ from features.delete_entry import remove
 from features.instructions import instruction
 from features.update import update_result
 from features.view_all import view_data
-from ui.banner import big_line, show_banner, show_line
+from ui.banner import show_banner, show_line
 from ui.colors import GREEN, RED, RESET, YELLOW
 
 show_banner()
@@ -19,7 +19,9 @@ def menu():
         print(f"(U) {GREEN}To Update the result.{RESET}")
         print(f"(Q) {RED}To Exit{RESET}")
         print(f"(H) {GREEN}Help?")
-        user_input = input(f"{YELLOW}Enter your choice[a, c, d, u, q]: {RESET}").lower()
+        user_input = input(
+            f"{YELLOW}Enter your choice[a, c, d, h, u, q]: {RESET}"
+        ).lower()
 
         if user_input == "c":
             check_result()  # Calls function inside check.py
